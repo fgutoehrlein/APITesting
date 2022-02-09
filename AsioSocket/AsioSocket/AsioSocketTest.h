5d43515947db0dca53c10778b6faa4dcea91f9b6
Felix Gutoehrlein <fgutoeh@emea.corpdir.net>
2022-02-09 08:50:39 +0100

    Asio as http server

--- AsioSocket/AsioSocket/AsioSocketTest.h
+++ AsioSocket/AsioSocket/AsioSocketTest.h
@@ -2,5 +2,14 @@
 
 #pragma once
 
-#include <iostream>
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
 #include <boost/asio.hpp>
+#include <chrono>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <memory>
+#include <string>
+#include "HttpConnection.h"
